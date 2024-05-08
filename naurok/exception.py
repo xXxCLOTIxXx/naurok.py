f6a5e1083ab7b5c9a5a96e27e3a8d1c459413a4e
Xsarz <88231084+xXxCLOTIxXx@users.noreply.github.com>
2024-05-08 23:28:53 +0300

    Delete naurok directory

--- naurok/exception.py
+++
@@ -1,9 +0,0 @@
-
-class LinkError(Exception):
-	def __init__(*args, **kwargs):
-		Exception.__init__(*args, **kwargs)
-
-
-class NotForwarded(Exception):
-	def __init__(*args, **kwargs):
-		Exception.__init__(*args, **kwargs)
